e17fc61bf8a6c6176326649792946f6a74b4bce0
Jorge Brunal Pérez <jorge.brunal@gmail.com>
2025-01-13 01:31:29 -0500

    tests for morse libs, with some examples :smile:

---
+++ tests/morse-lib.rs
@@ -0,0 +1,21 @@
+#[test]
+fn use_encode_spanish() {
+    match morse::encode::encode("El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña ¡tocaba el saxofón detrás del palenque de paja!.") {
+        Ok(x) => println!("{}", x),
+        Err(e) => {
+            println!("The following chars were unsupported {:?}",
+                     e.unsupported_characters)
+        }
+    }
+}
+
+#[test]
+fn use_decode_spanish() {
+    match morse::decode::decode(". ._.. / ..._ . ._.. ___ __.. / __ .._ ._. _._. .. .._.. ._.. ._ __. ___ / .... .. _. _.. .._ / _._. ___ __ .. ._ / .._. . ._.. .. __.. / _._. ._ ._. _.. .. ._.. ._.. ___ / _.__ / _._ .. .__ .. ._._._ / ._.. ._ / _._. .. __. ..__ . __.__ ._ / __..._ _ ___ _._. ._ _... ._ / . ._.. / ... ._ _.._ ___ .._. ___. _. / _.. . _ ._. .__._ ... / _.. . ._.. / .__. ._ ._.. . _. __._ .._ . / _.. . / .__. ._ .___ ._ _._.__ ._._._") {
+        Ok(x) => println!("{}", x),
+        Err(e) => {
+            println!("The following chars were unsupported {:?}",
+                     e.unsupported_characters)
+        }
+    }
+}
